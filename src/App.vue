456c4a0bbdb09649bda2512a209d581bc536ce94
gemi9119 <gemi9119@gmail.com>
2024-11-12 22:04:34 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useRoute } from 'vue-router';
 import Menu from './components/menu.vue'
+// import navMenu from './components/navMenu.vue'
+import sideMenu from './components/sideMenu.vue'
 const route = useRoute();
 </script>
 <template>
+  <sideMenu/>
+  <!-- <navMenu/> -->
   <RouterView />
   <Menu v-if="route.path !== '/'"/>
 </template>
